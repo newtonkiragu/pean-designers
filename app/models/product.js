826c35d599ddd462fff9cbf427df3f488193849c
pmutua <pmutua@live.com>
2017-06-14 12:56:02 +0300

    add product tile component

--- app/models/product.js
+++ app/models/product.js
@@ -1,9 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	  id: DS.attr(),
+	  // id: DS.attr(),
       name:DS.attr(),
       brandedName: DS.attr(),
-      
-
+      brandedName: DS.attr(),
+      unbrandedName: DS.attr(),
+      currency: DS.attr(),
+      priceLabel: DS.attr()
 });
