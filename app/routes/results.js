57921aad6c571af08e1594810ba6b276d3f1c7c0
pmutua <pmutua@live.com>
2017-06-14 16:01:47 +0300

    update results.jsimport key

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Route.extend({
 
 model: function(params) {
-   var url = 'http://api.shopstyle.com/api/v2/products?pid=uid4089-39490717-44&fts='+params.name+'&offset=0&limit=10' ;
+   var key = config.myApiKey;
+   var url = 'http://api.shopstyle.com/api/v2/products?pid=' +key+ &fts='+params.name+'&offset=0&limit=10' ;
    return Ember.$.getJSON(url).then(function(responseJSON) {
    	//use console log this to identify the objects 
    	console.log(responseJSON.products);
