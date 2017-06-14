537f15985e739bdece421ba7e7ffee163501e43a
pmutua <pmutua@live.com>
2017-06-14 11:59:46 +0300

    updated index.hbs for the rendering of model attributes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
-
+var products= [
+    {
+      "id": 455813874,
+      "name": "Lanvin Stretch-scuba jersey dress",
+      "brandedName": "Lanvin Stretch-scuba jersey dress",
+      "unbrandedName": "Stretch-scuba jersey dress",
+      "currency": "USD",
+      "priceLabel": "$2,580.00",
+      "inStock": true,
+      }];
 export default Ember.Route.extend({
 });
