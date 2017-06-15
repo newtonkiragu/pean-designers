ac9b960ea5c3b6b98c7979688507e3fc5d086c4e
pmutua <pmutua@live.com>
2017-06-15 10:58:19 +0300

    And a corresponding action on the component:

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-});
+  shoppingCart: Ember.inject.service(),
+
+  actions: {
+    addToCart(item) {
+      this.get('shoppingCart').add(item);
+    },
